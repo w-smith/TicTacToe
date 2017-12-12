316a8df65fc8de22d8f71bf75910da69ae2cd6be
w-smith <wsmith@bex.net>
2017-12-12 11:30:25 -0700

    added win conditions

--- js/main.js
+++ js/main.js
@@ -12,20 +12,116 @@ var turn=0;
 
       $(".square").on("click", function(){
       	// console.log(this);
-        if ($(this).html() === '')
+        if 
+          ($(this).html() === '')
             if (turn % 2 === 0){
               $(this).text('@'); 
             } else {
               $(this).text('#');
-
             }
+
+// @ top row
+    if 
+          ( ($(".1").html() == "@") && ($(".2").html() == "@") && ($(".3").html() == "@") ){
+            alert("Game over! @ wins!");
+          }
+// @ middle row
+    else if 
+          ( ($(".4").html() == "@") && ($(".5").html() == "@") && ($(".6").html() == "@") ){
+            alert("Game over! @ wins!");
+          }
+// @ bottom row        
+    else if 
+          ( ($(".7").html() == "@") && ($(".8").html() == "@") && ($(".9").html() == "@") ){
+            alert("Game over! @ wins!");
+          }
+// @ left column          
+    else if 
+          ( ($(".1").html() == "@") && ($(".4").html() == "@") && ($(".7").html() == "@") ){
+            alert("Game over! @ wins!");
+          }
+// @ middle column          
+    else if 
+          ( ($(".2").html() == "@") && ($(".5").html() == "@") && ($(".8").html() == "@") ){
+            alert("Game over! @ wins!");
+          }
+// @ right column          
+    else if 
+          ( ($(".3").html() == "@") && ($(".6").html() == "@") && ($(".9").html() == "@") ){
+            alert("Game over! @ wins!");
+          }
+// @ left to right diagonal          
+    else if 
+          ( ($(".1").html() == "@") && ($(".5").html() == "@") && ($(".9").html() == "@") ){
+            alert("Game over! @ wins!");
+          }
+// @ right to left diagonal          
+    else if 
+          ( ($(".3").html() == "@") && ($(".5").html() == "@") && ($(".7").html() == "@") ){
+            alert("Game over! @ wins!");
+          }
+
+
+// # top row
+    else if 
+          ( ($(".1").html() == "#") && ($(".2").html() == "#") && ($(".3").html() == "#") ){
+            alert("Game over! # wins!");
+          }
+// # middle row
+    else if 
+          ( ($(".4").html() == "#") && ($(".5").html() == "#") && ($(".6").html() == "#") ){
+            alert("Game over! # wins!");
+          }
+// # bottom row        
+    else if 
+          ( ($(".7").html() == "@") && ($(".8").html() == "#") && ($(".9").html() == "#") ){
+            alert("Game over! # wins!");
+          }
+// # left column          
+    else if 
+          ( ($(".1").html() == "#") && ($(".4").html() == "#") && ($(".7").html() == "#") ){
+            alert("Game over! # wins!");
+          }
+// # middle column          
+    else if 
+          ( ($(".2").html() == "#") && ($(".5").html() == "#") && ($(".8").html() == "#") ){
+            alert("Game over! # wins!");
+          }
+// # right column          
+    else if 
+          ( ($(".3").html() == "#") && ($(".6").html() == "#") && ($(".9").html() == "#") ){
+            alert("Game over! # wins!");
+          }
+// # left to right diagonal          
+    else if 
+          ( ($(".1").html() == "#") && ($(".5").html() == "#") && ($(".9").html() == "#") ){
+            alert("Game over! # wins!");
+          }
+// # right to left diagonal          
+    else if 
+          ( ($(".3").html() == "#") && ($(".5").html() == "#") && ($(".7").html() == "#") ){
+            alert("Game over! # wins!");
+          }
+
+
           turn++;
           
+
+
+          
+
+         
+
       });
 
     $('#button').click(function() {
     location.reload();
     });
+
+
+
+
+
 
 // // Top Left
 // $("#tl").click(function() {
